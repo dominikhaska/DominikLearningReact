a7790c9777f506709b9482d7d57f47070708be72
Dominik Haska <dominik.haska@jeppesen.com>
2018-08-04 22:31:32 +0200

    chapter 4 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-      console.log("this should stop here");
+import MyButton from "./MyButton";
 
-     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React DHA </h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to  reload.
-        </p>
-      </div>
-    );
-  }
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <MyButton>Click Me</MyButton>
+        );
+    }
+
+    render2() {
+        console.log("this should stop here");
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React DHA </h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+
 }
 
 export default App;
