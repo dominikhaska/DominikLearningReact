0e55eccf4f1b72aaa31364fe5496be174db16140
Dominik Haska <dominik.haska@jeppesen.com>
2018-08-17 11:57:29 +0200

    chapter 8 - inheriting state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import MyComponent from "./MyComponent";
 
 render((
         <section>
-
+            <MyComponent />
         </section>
     ),
     document.getElementById('root')
